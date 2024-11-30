efbf0a5a77a066ff79918a5100bcf5ac210290b0
tildejustin <spacepepper3.14@gmail.com>
2024-11-30 14:43:03 -0500

    more complete noshare impl, sharing intraprocess doesn't work though so cannot read world list after leaving world

--- src/main/java/dev/tildejustin/example/mixin/LevelStorage$SessionMixin.java
+++ src/main/java/dev/tildejustin/example/mixin/LevelStorage$SessionMixin.java
@@ -1,41 +1,67 @@
 package dev.tildejustin.example.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.*;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import com.sun.nio.file.ExtendedOpenOption;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.util.registry.RegistryTracker;
-import net.minecraft.world.SaveProperties;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.channels.SeekableByteChannel;
 import java.nio.file.*;
 
 @Mixin(LevelStorage.Session.class)
 public abstract class LevelStorage$SessionMixin {
-    @Shadow
-    @Final
-    private Path directory;
+    @Unique
+    private SeekableByteChannel levelDataFile;
 
     @Unique
-    private SeekableByteChannel leakedHandle;
+    private SeekableByteChannel levelDataBackupFile;
 
-    @Inject(
-            method = "backupLevelDataFile(Lnet/minecraft/util/registry/RegistryTracker;Lnet/minecraft/world/SaveProperties;Lnet/minecraft/nbt/CompoundTag;)V",
-            at = @At(value = "RETURN")
-    )
-    private void lockLevelDat(RegistryTracker registryTracker, SaveProperties saveProperties, CompoundTag compoundTag, CallbackInfo ci) throws IOException {
-        Path file = this.directory.resolve("level.dat");
-        if (leakedHandle == null) {
-            leakedHandle = Files.newByteChannel(
-                    file,
-                    StandardOpenOption.WRITE,
-                    ExtendedOpenOption.NOSHARE_READ,
-                    ExtendedOpenOption.NOSHARE_WRITE,
-                    ExtendedOpenOption.NOSHARE_DELETE
-            );
+    @Unique
+    private final OpenOption[] options = new OpenOption[]{StandardOpenOption.READ, StandardOpenOption.WRITE, StandardOpenOption.CREATE, ExtendedOpenOption.NOSHARE_READ, ExtendedOpenOption.NOSHARE_WRITE};
+
+    @WrapOperation(method = "backupLevelDataFile(Lnet/minecraft/util/registry/RegistryTracker;Lnet/minecraft/world/SaveProperties;Lnet/minecraft/nbt/CompoundTag;)V", at = @At(value = "INVOKE", target = "Ljava/io/File;createTempFile(Ljava/lang/String;Ljava/lang/String;Ljava/io/File;)Ljava/io/File;"))
+    private File noTempFile(String prefix, String suffix, File dir, Operation<File> original) {
+        return null;
+    }
+
+    @SuppressWarnings("InvalidInjectorMethodSignature")
+    @WrapOperation(method = "backupLevelDataFile(Lnet/minecraft/util/registry/RegistryTracker;Lnet/minecraft/world/SaveProperties;Lnet/minecraft/nbt/CompoundTag;)V", at = @At(value = "NEW", target = "java/io/FileOutputStream"))
+    private @Coerce OutputStream replaceOutputStream(File file, Operation<FileOutputStream> original, @Share("output") LocalRef<ByteArrayOutputStream> outputStream) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        outputStream.set(byteArrayOutputStream);
+        return byteArrayOutputStream;
+    }
+
+    @WrapOperation(method = "backupLevelDataFile(Lnet/minecraft/util/registry/RegistryTracker;Lnet/minecraft/world/SaveProperties;Lnet/minecraft/nbt/CompoundTag;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util;backupAndReplace(Ljava/io/File;Ljava/io/File;Ljava/io/File;)V"))
+    private void secureReplace(File dat, File temp, File bak, Operation<Void> original, @Share("output") LocalRef<ByteArrayOutputStream> outputStream) throws IOException {
+        if (levelDataFile == null || Files.notExists(dat.toPath())) {
+            if (levelDataFile != null) {
+                levelDataFile.close();
+            }
+            levelDataFile = Files.newByteChannel(dat.toPath(), options);
         }
+
+        if (levelDataBackupFile == null) {
+            levelDataBackupFile = Files.newByteChannel(bak.toPath(), options);
+        }
+
+        if (Files.exists(dat.toPath())) {
+            levelDataBackupFile.truncate(0);
+            levelDataBackupFile.position(0);
+            ByteBuffer buffer = ByteBuffer.allocate((int) Files.size(dat.toPath()));
+            levelDataFile.read(buffer);
+            levelDataBackupFile.write(buffer);
+        }
+
+        // riskier than the "write to temp, rename file" strategy that vanilla uses
+        // may want to consider reimplementing that, although hard to do while keeping handles valid
+        levelDataFile.truncate(0);
+        levelDataFile.position(0);
+        levelDataFile.write(ByteBuffer.wrap(outputStream.get().toByteArray()));
     }
 }
